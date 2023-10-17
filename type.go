5a2dedf3ddafc0d7d9f6cf0ca1a5231cf7954310
dani ferdinan <daniferdinandall@gmail.com>
2023-10-17 14:35:04 +0700

    first commit

---
+++ type.go
@@ -0,0 +1,13 @@
+package betrensentimen
+
+type User struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+	Role     string `json:"role,omitempty" bson:"role,omitempty"`
+}
+
+type Credential struct {
+	Status  bool   `json:"status" bson:"status"`
+	Token   string `json:"token,omitempty" bson:"token,omitempty"`
+	Message string `json:"message,omitempty" bson:"message,omitempty"`
+}
